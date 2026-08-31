a7d16aa5bbdb2add49a10cc887de63c762a2433a
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2026-08-31 10:20:54 +0200

    Don't introduce a Rust function on top of the function we want to bind

--- exercises/01_intro/01_what_is_ffi/src/lib.rs
+++ exercises/01_intro/01_what_is_ffi/src/lib.rs
@@ -1,17 +1,15 @@
-fn add(a: i32, b: i32) -> i32 {
-    // write an `extern "C" {}` block declaring the `bm_add` function we want to bind to
-    // then call this function here to implement the add!
-
-    todo!()
+unsafe extern "C" {
+    // TODO: add an entry to this `extern "C" {}` block to allow our tests
+    // below to invoke the `bm_add` C function defined in `c_src/bm_legacy.*`.
 }
 
 #[cfg(test)]
 mod tests {
-    use super::add;
+    use super::bm_add;
 
     #[test]
     fn test_add() {
-        assert_eq!(add(2, 3), 5);
-        assert_eq!(add(0, 0), 0);
+        assert_eq!(unsafe { bm_add(2, 3) }, 5);
+        assert_eq!(unsafe { bm_add(0, 0) }, 0);
     }
 }
